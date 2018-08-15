8251aa17e1fd25f349efa3aad1f39faa3b9f22d8
João Pedro Porto Pires de Oliveira <joaopppobr@gmail.com>
2018-08-15 01:24:25 -0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Lab1ED.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1ED.dir/main.c.o"
+  "CMakeFiles/Lab1ED.dir/jogadores.c.o"
+  "Lab1ED.pdb"
+  "Lab1ED"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab1ED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
